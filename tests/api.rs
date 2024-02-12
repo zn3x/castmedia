ff91bdfb3ebe86a1483050e34758e8c30a72ffff
zesty <gaggg@tutanota.com>
2024-02-12 19:43:19 +0000

    Fixing tests && Adding initial public api test

--- tests/api.rs
+++ tests/api.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
-const CONFIG: &str = "
+const CONFIG_ADMIN_API: &str = "
 address:
   - bind: 127.0.0.1:9000
 account:
@@ -26,10 +26,38 @@ admin_access:
     bind: 127.0.0.1:9100
 ";
 
+const CONFIG_PUBLIC_API: &str = "
+address:
+  - bind: 127.0.0.1:9001
+info:
+  id: test1
+  admin: test2
+  location: test3
+  description: test4
+account:
+  admin:
+    pass: 0$pass
+    role: admin
+  source:
+    pass: 0$pass
+    role: source
+    mount:
+      - path: '/stream.mp3'
+admin_access:
+  enabled: true
+  address:
+    bind: 127.0.0.1:9101
+";
+
 static TEST_DIR: &str     = env!("CARGO_TARGET_TMPDIR");
 
+// admin_api
 const BASE: &str          = "127.0.0.1:9000";
 const ADMIN: &str         = "127.0.0.1:9100";
+
+// public_api
+const BASE1: &str         = "127.0.0.1:9001";
+const ADMIN1: &str        = "127.0.0.1:9101";
 
 const AUTH_ADMIN: &str    = "admin:pass";
 const AUTH_SOURCE: &str   = "source:pass";
@@ -132,24 +160,39 @@ async fn assert_fallback(mount: &str, fallback: Option<&str>) {
     assert_eq!(fallback, pfallback);
 }
 
-#[tokio::test]
-async fn admin_api() {
-    tracing_subscriber::fmt().with_thread_names(true).with_max_level(tracing::Level::DEBUG).init();
-    let server = spawn_server(CONFIG, "perm.yaml").await;
-
-    tokio::time::sleep(Duration::from_secs(2)).await;
-
-    let mut source = tokio::process::Command::new("ffmpeg")
+async fn spawn_source(auth: &str, addr: &str, mount: &str) -> tokio::process::Child {
+    tokio::process::Command::new("ffmpeg")
         .args([
               "-loglevel", "panic",
               "-re", "-f", "lavfi",
               "-i", "sine=frequency=1000",
               "-content_type", "audio/mpeg",
               "-vn", "-f", "mp3",
-              &format!("icecast://{}@{}{}", AUTH_SOURCE, ADMIN, MOUNT_SOURCE)
+              &format!("icecast://{}@{}{}", auth, addr, mount)
         ])
         .spawn()
-        .expect("ffmpeg missing");
+        .expect("ffmpeg missing")
+}
+
+async fn spawn_listener(addr: &str, mount: &str) -> tokio::process::Child {
+    tokio::process::Command::new("curl")
+        .args([
+              "-o", "/dev/null",
+              "-L",
+              "-H \"Icy-Metadata: 1\"",
+              &format!("http://{}{}", addr, mount)
+        ])
+        .spawn()
+        .expect("curl missing")
+}
+
+#[tokio::test]
+async fn admin_api() {
+    let server = spawn_server(CONFIG_ADMIN_API, "admin_api.yaml").await;
+
+    tokio::time::sleep(Duration::from_secs(2)).await;
+
+    let mut source = spawn_source(AUTH_SOURCE, ADMIN, MOUNT_SOURCE).await;
 
     let mut r;
     // Checking admin api services that only admin should be able to reach
@@ -225,6 +268,69 @@ async fn admin_api() {
     r = get_status_code(&format!("http://{}@{}/admin/fallbacks?mount={}&fallback={}", AUTH_SOURCE, ADMIN, MOUNT_SOURCE, MOUNT_SOURCE1)).await;
     assert_eq!(r, 200);
     assert_fallback(MOUNT_SOURCE, Some(MOUNT_SOURCE1)).await;
+
+    source.kill().await.ok();
+    drop(server);
+}
+
+async fn get_stat(path: &str, key: &str) -> serde_json::Value {
+    let resp = get_response(&format!("http://{}{}", BASE1, path)).await;
+    assert_eq!(resp.status().as_u16(), 200);
+    let body = resp
+        .json::<serde_json::Value>()
+        .await
+        .unwrap();
+    let body = body.as_object()
+        .unwrap();
+    let value = body.get(key)
+        .unwrap()
+        .clone();
+
+    value
+}
+
+#[tokio::test]
+async fn public_api() {
+    let start = chrono::offset::Utc::now().timestamp();
+
+    let server = spawn_server(CONFIG_PUBLIC_API, "public_api.yaml").await;
+
+    tokio::time::sleep(Duration::from_secs(2)).await;
+
+    let stats = get_stat("/api/serverinfo", "stats").await;
+    let s     = stats.as_object()
+        .unwrap();
+
+    assert_eq!(0, s.get("active_listeners").unwrap().as_u64().unwrap());
+    assert_eq!(0, s.get("peak_listeners").unwrap().as_u64().unwrap());
+    assert!(s.get("start_time").unwrap().as_i64().unwrap() >= start);
+    assert!(s.get("start_time").unwrap().as_i64().unwrap() <= chrono::offset::Utc::now().timestamp());
+
+    let stats = get_stat("/api/serverinfo", "properties").await;
+    let s     = stats.as_object()
+        .unwrap();
+    assert_eq!("test1", s.get("id").unwrap().as_str().unwrap());
+    assert_eq!("test2", s.get("admin").unwrap().as_str().unwrap());
+    assert_eq!("test3", s.get("location").unwrap().as_str().unwrap());
+    assert_eq!("test4", s.get("description").unwrap().as_str().unwrap());
+
+    let stats = get_stat("/api/serverinfo", "mounts").await;
+    assert!(
+        stats.as_array().unwrap().is_empty()
+    );
+
+    let mut source = spawn_source(AUTH_SOURCE, ADMIN1, MOUNT_SOURCE).await;
+    
+    tokio::time::sleep(Duration::from_secs(2)).await;
+
+    let stats = get_stat("/api/serverinfo", "mounts").await;
+    assert_eq!(1, stats.as_array().unwrap().len());
+    assert!(
+        stats.as_array().unwrap().first().unwrap().as_str().eq(&Some(MOUNT_SOURCE))
+    );
+
+    let listener  = spawn_listener(BASE1, MOUNT_SOURCE).await;
+    let listener1 = spawn_listener(BASE1, MOUNT_SOURCE).await;
 
     source.kill().await.ok();
     drop(server);
