461e8fad0a71f337fcd1ac4275c136c2346ed7fb
zesty <gaggg@tutanota.com>
2024-03-16 01:42:03 +0000

    Fine grained testing for icecast media stream

--- test-utils/src/lib.rs
+++ test-utils/src/lib.rs
@@ -1,3 +1,7 @@
+use core::panic;
+use std::{net::TcpStream, io::{Write, Read}, process::Stdio};
+
+use base64::Engine;
 pub use reqwest;
 
 pub struct Server {
@@ -56,6 +60,49 @@ pub async fn spawn_source(auth: &str, addr: &str, mount: &str) -> tokio::process
         ])
         .spawn()
         .expect("ffmpeg missing")
+}
+
+pub fn spawn_source_manual(auth: &str, addr: &str, mount: &str) -> (TcpStream, std::process::Child) {
+    let mut sock = TcpStream::connect(addr)
+        .expect("Should be able to connect");
+
+    let bs64 = base64::engine::general_purpose::URL_SAFE;
+
+    sock.write_all(format!("SOURCE {} HTTP/1.1\r\nHost: {}\r\nContent-Type: audio/mpeg\r\nAuthorization: Basic {}\r\nConnection: close\r\nIcy-Metadata: 1\r\nIce-Public: 1\r\n\r\n",
+                       mount, addr, bs64.encode(auth)).as_bytes())
+        .expect("Should be able to write");
+
+
+    let mut buf     = [0u8; 1];
+    let mut headers = Vec::new();
+    loop {
+        sock.read_exact(&mut buf).expect("Can't read");
+        headers.push(buf[0]);
+
+        if headers.windows(4).last() == Some(b"\r\n\r\n") {
+            break;
+        }
+    };
+
+    if !headers.starts_with(b"HTTP/1.1 200 OK") {
+        panic!("Source: got bad response");
+    }
+
+    let media = std::process::Command::new("ffmpeg")
+        .args([
+              "-loglevel", "panic",
+              "-f", "lavfi",
+              "-i", "sine=frequency=1000",
+              "-content_type", "audio/mpeg",
+              "-vn", "-f", "mp3",
+              "-b:a", "320k",
+              "-"
+        ])
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("ffmpeg missing");
+
+    (sock, media)
 }
 
 pub async fn spawn_listener(addr: &str, mount: &str) -> tokio::process::Child {
